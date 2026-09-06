88028563dd27fc82547677ee9fda4a56412db2e9
Mehmet Boztepe <mehmet.boztepe.tech@gmail.com>
2026-09-06 14:58:03 +0300

    stage-4: expose map and collision engine modules

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,9 +1,12 @@
+pub mod collision;
 pub mod config;
 pub mod entity;
+pub mod map;
 pub mod math;
 pub mod rules;
 
 pub use config::GameConfig;
 pub use entity::{GameState, Player, PlayerId, Projectile, ProjectileId, Tank, TeamId};
+pub use map::{GameMap, MapSize, Wall};
 pub use math::Vec2;
-pub use rules::GameRules;
+pub use rules::{GameMode, GameRules};
