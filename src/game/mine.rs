8b0b58eac65796148b71ead7b36f9a024adbd4c1
Mehmet Boztepe <mehmet.boztepe.tech@gmail.com>
2026-09-11 08:17:31 +0300

    style: normalize mine formatting

--- src/game/mine.rs
+++ src/game/mine.rs
@@ -1,4 +1,4 @@
-use super::{GameMap, GameState, PlayerId, Vec2, collision};
+use super::{collision, GameMap, GameState, PlayerId, Vec2};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Mine {
