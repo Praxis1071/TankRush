4ebd0fac41ec47ab866f25f91c5801b2477a957a
Mehmet Boztepe <mehmet.boztepe.tech@gmail.com>
2026-09-11 08:18:18 +0300

    style: normalize simulation formatting

--- src/game/simulation.rs
+++ src/game/simulation.rs
@@ -1,4 +1,4 @@
-use super::{GameConfig, GameMap, GameState, PlayerId, Vec2, collision::segment_wall_hit};
+use super::{collision::segment_wall_hit, GameConfig, GameMap, GameState, PlayerId, Vec2};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub struct TankInput {
